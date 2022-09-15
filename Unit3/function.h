c9bdd2f97ab201a48ad74d2bb2488d28792b9cab
hiyoung <a2440914921@gmail.com>
2022-09-15 21:24:23 +0800

    Unit3

--- Unit3/function.h
+++ Unit3/function.h
@@ -2,14 +2,12 @@
 #include "var.h"
 
 //简单数组实现表
-int Arr_PrintList(Arrtable table)//打印表元素 OK
+void Arr_PrintList(Arrtable table)//打印表元素 OK
 {
-    for(int i=0; i<table.length; i++)
+    for(int i=0; i < table.length; i++)
     {
-        if(table.arr[i] == '\0'){ //pointer should be '\0',int should be NULL
-            printf("Not Initialized");
-            break;
-        }
+        if(table.arr[i] == '\0') //pointer should be '\0',int should be NULL
+            exit(0);
         else 
             printf("Array[%d]:%d \n",i,table.arr[i]);
     }
@@ -33,7 +31,7 @@ Arrtable Arr_MakeEmpty()//初始化空表  OK
     return t;
 }
 
-void Arr_Find(Arrtable table,int n)//返回关键字首次出现的位置  OK
+void Arr_Find(Arrtable table,int n)//返回元素首次出现的位置  OK
 {
     int found;
     for(int i=0; i<table.length; i++){
@@ -47,12 +45,46 @@ void Arr_Find(Arrtable table,int n)//返回关键字首次出现的位置  OK
         printf("NOT FOUND Number %d\n",n);
 }
 
-int Arr_Insert()//在某位置插入元素
+void Arr_Insert(Arrtable table,int InsertPos,int Element)//在某位置插入元素 OK
 {
-    
+    if (InsertPos < 0 || InsertPos > table.length) //插入位置是否合理
+    {
+        printf("Array Insertion Position Error");
+        exit(0);
+    }    
+
+    if(table.length == table.size)
+    {
+        table.arr = (int*)realloc(table.arr,(table.size + 1)*sizeof(int));
+        if (!table.arr)
+        {
+            printf("Error");
+            exit(0);
+        }
+        table.arr += 1;
+    }
+
+    for(int i = table.length-1; i >= InsertPos-1; i--)
+    {
+        table.arr[i+1] = table.arr[i];
+    }
+    table.arr[InsertPos-1] = Element;
+
+    table.length++;
 }
 
-int Arr_Dekete()//删除表中某元素
+void Arr_Delete(Arrtable table,int DelDataPos)//删除表中某位置元素 OK
 {
+    if(DelDataPos < 0 || DelDataPos > table.length)//删除位置是否合理
+    {
+        printf("Array Delete Position Error");
+        exit(0);
+    }
 
+    for (int i = DelDataPos; i < table.length; i++)
+    {   
+        table.arr[i-1]=table.arr[i];
+    }
+    table.arr[table.length-1] = 0;
+    table.length--;
 }
--- Unit3/main.c
+++ Unit3/main.c
@@ -6,15 +6,26 @@ int main(void)
     //test OK
     // int arr[Arr_N]={1,2,3,4,5,6,7,8,9,10};
     // Arr_PrintList(arr,Arr_N);
-    int n = 10;
+    int n = 4; //传入元素
     Arrtable t = Arr_MakeEmpty();
     for(int i=0;i<t.size;i++)
     {
         t.arr[i]=i+1;
         t.length++;
     }
+    printf("原数组\n");
     Arr_PrintList(t);
+
+    printf("查找某元素第一次出现的位置\n");
     Arr_Find(t,n);
+
+    printf("插入元素\n");
+    Arr_Insert(t,n,10);
+    Arr_PrintList(t);
+
+    printf("删除元素\n");
+    Arr_Delete(t,n);
+    Arr_PrintList(t);
 
     return 0;
 }
