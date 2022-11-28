71826d13d5f6e469ed7be538451e991570e6bfab
Hiyoung <a2440914921@gmail.com>
2022-11-28 16:48:51 +0800

    2022-11-28
    合并var和function

--- Unit3/Array/function.h
+++ Unit3/Array/function.h
@@ -1,5 +1,14 @@
 #include <stdlib.h>
-#include "var.h"
+
+#define ARR_N 10 //数组大小
+ 
+typedef struct Arrtable //顺序表的数据结构
+{
+    int * arr;//创建一个名为Arr的数组
+    int length;//记录当前顺序表的长度
+    int size;//记录顺序表定义的最大容量
+}Arrtable;
+
 
 //简单数组实现表
 void Arr_PrintList(Arrtable table)//打印表元素 OK
--- Unit3/Array/var.h
+++
@@ -1,8 +0,0 @@
-#define ARR_N 10 //数组大小
- 
-typedef struct Arrtable //顺序表的数据结构
-{
-    int * arr;//创建一个名为Arr的数组
-    int length;//记录当前顺序表的长度
-    int size;//记录顺序表定义的最大容量
-}Arrtable;
